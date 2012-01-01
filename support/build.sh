3a5cc60fc1db1497d53b0733bbadf64e78fda0f6
Tony Heupel <tony.heupel@infospace.com>
2012-01-01 09:16:08 -0800

    add minified javascript using closure compiler

--- support/build.sh
+++ support/build.sh
@@ -10,3 +10,6 @@ browserify --require liquify -o ./public/javascripts/liquify.js
 echo 'global = window;\n' > public/javascripts/nl.js 
 cat public/javascripts/liquify.js >> public/javascripts/nl.js
 mv public/javascripts/nl.js public/javascripts/liquify.js
+
+# minify/obfuscate liquify.js
+java -jar ./support/closure.jar --js ./public/javascripts/liquify.js --js_output_file ./public/javascripts/liquify.min.js
